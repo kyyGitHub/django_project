36e1247fcbd7f9b8c2d19fd6913f1889ba8acfd4
kyyGitHub <1817265@donga.ac.kr>
2022-08-16 11:26:06 +0900

    포스트 목록, 상세 페이지에서 작성자 정보 출력하기

--- blog/tests.py
+++ blog/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase, Client
 from bs4 import BeautifulSoup
+from django.contrib.auth.models import User
 from .models import Post
 
 class Testview(TestCase):
     def setUp(self):
         self.client = Client()
+        self.user_naruto = User.objects.create_user(username='naruto', password='somepassword')
+        self.user_itachi = User.objects.create_user(username='itachi', password='somepassword')
 
     def navbar_test(self, soup):
         navbar = soup.nav
@@ -36,13 +39,17 @@ class Testview(TestCase):
         self.assertIn('아직 게시물이 없습니다', main_area.text)
 
         post_001 = Post.objects.create(
-            title = '첫번째 포스트입니다.',
-            content = 'Hello world. We are the world.',
+            title='첫번째 포스트입니다.',
+            content='Hello world. We are the world.',
+            author=self.user_naruto
         )
+
         post_002 = Post.objects.create(
             title='두번째 포스트입니다.',
             content='1등이 전부는 아니잖아요?',
+            author=self.user_itachi
         )
+
         self.assertEqual(Post.objects.count(),2)
 
         response = self.client.get('/blog/')
@@ -55,12 +62,19 @@ class Testview(TestCase):
 
         self.assertNotIn('아직 게시물이 없습니다', main_area.text)
 
+        self.assertIn(post_001.author.username.upper(), main_area.text)
+        self.assertIn(post_002.author.username.upper(), main_area.text)
+
+
     def test_post_detail(self):
             post_001 = Post.objects.create(
                 title='첫번째 포스트입니다.',
                 content='Hello world. We are the world.',
+                author=self.user_naruto,
             )
             self.assertEqual(post_001.get_absolute_url(),'/blog/1/')
+
+            self.assertIn(post_001.author.username.upper(), post_area.text)
 
             response = self.client.get(post_001.get_absolute_url())
             self.assertEqual(response.status_code, 200)
